69b76003d3b4cdab166a252e0cccbd61e32944f8
Dâng <dangbui@coinmap.tech>
2022-09-17 11:21:42 +0700

    feat: new build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default function Document() {
   return (
     <Html lang="vn">
-      <Head />
+      <Head>
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Webreview" />
+        <meta property="og:url" content={`https://webreview.click`} />
+        <meta
+          property="og:title"
+          content="Webreview | Contet | SEO | Marketing"
+        />
+        <meta
+          property="og:description"
+          content="Webreview | chiến lược marketing | SEO |  Top"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
