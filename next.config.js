c2c16b2c7066ccd4ab527184a75d1db185b57efd
Dâng <dangbui@coinmap.tech>
2022-10-13 09:37:31 +0700

    new build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const IMAGES_DOMAINS = process.env.IMAGES_DOMAINS.split(',');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: IMAGES_DOMAINS,
+    domains: ['samartheme1.vercel.app', 'localhost', 'admin.webchiase.online'],
   },
   env: {
     BASE_URL: process.env.BASE_URL,
